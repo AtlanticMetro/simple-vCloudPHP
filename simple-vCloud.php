d0dfdbf265b67ab321bd078a1aa964315275bd1f
James <james@gozunga.com>
2013-02-11 19:00:36 -0500

    refactor

--- simple-vCloud.php
+++ simple-vCloud.php
@@ -6,7 +6,7 @@
 
 // Get an array of an organization's VDCs. This is their object, of type ..
 
-function getVdcs($sdkOrg, $service, $orgName)
+function getVdcs($sdkOrg, $service)
 {
     
     // create an SDK vDC object
@@ -82,7 +82,159 @@ function adminLogin($user, $pswd, $server, $orgName, &$service)
     return $sdkOrg;    
 }
 
+/* ************************************************************************** */
 
+function getVdcInfo($vdc)
+{
+    $computeCapacity = $vdc->getComputeCapacity();       
+    $cpuDetails = $computeCapacity->getCpu();
+    $ramDetails = $computeCapacity->getMemory();
+
+    $vdcInfo = array(
+                     
+                    'name' => $vdc->get_name(),
+                    'description' => $vdc->getDescription(),
+                    'status' => $vdc->getIsEnabled(),
+                    'allocation_model' => $vdc->getAllocationModel(),
+                    'maximum_vm' => $vdc->getVmQuota(),
+                    'compute_capacity' => array(
+                        'cpu_size' => $vdc->getVCpuInMhz(), // in Mhz
+                        'cpu_used' => $cpuDetails->getUsed(),
+                        'cpu_overhead' => $cpuDetails->getOverhead(),
+                        'cpu_reserved' => $cpuDetails->getReserved()
+                    ),
+                    'memory_capacity' => array(
+                        'memory_used' => $ramDetails->getUsed(),
+                        'memory_overhead' => $ramDetails->getOverhead(),
+                        'memory_reserved' => $ramDetails->getReserved()
+                    )
+    );
+    
+    
+    return $vdcInfo;
+}
+
+/* ************************************************************************** */
+
+function getVdcResources($vCloud,$vdc)
+{
+        
+    $resources = $vdc->getResourceEntities();
+
+    $vApps = array();
+    $vAppTemplates = array();
+    $media = array();
+        
+    foreach($resources->getResourceEntity() as $obj)
+    {
+    //ifDebug("Resource Type: [" . $obj->get_type() . "]");
+
+        if(preg_match("/\.vApp\+/",$obj->get_type()))
+        {
+            $curVapp = null;
+
+            $sdkvApp = $vCloud->createSDKObj($obj->get_href());
+        
+            $vApp = $sdkvApp->getVApp();
+            $curVapp['name'] = $vApp->get_name();
+            $curVapp['totals']['cpu'] = 0;
+            $curVapp['totals']['memory'] = 0;
+            $curVapp['totals']['storage'] = 0;
+            
+            $myVms = array();
+            
+            $children = $vApp->getChildren();
+            
+            foreach($children->getVm() as $vm)
+            {
+
+                $curVm['name'] = $vm->get_name();
+                $curVm['href'] = $vm->get_href();
+                $curVm['type'] = $vm->get_type();
+                
+                $curVm['totals']['cpu'] = 0;                
+                $curVm['totals']['memory'] = 0;
+                $curVm['totals']['storage'] = 0;
+                
+                $sdkVm = $vCloud->createSDKObj($vm->get_href());
+
+
+                $cpuZ = $sdkVm->getVirtualCpu();
+                $cpuY = $cpuZ->getVirtualQuantity();
+                $curVm['cpu'] = $cpuY->valueOf;
+                
+                $memZ = $sdkVm->getVirtualMemory();
+                $memY = $memZ->getVirtualQuantity();
+                $curVm['memory'] = $memY->valueOf;            
+           
+                $curVm['totals']['cpu'] = $curVm['cpu'];
+                $curVm['totals']['memory'] = $curVm['memory'];
+
+                $curVapp['totals']['cpu'] += $curVm['cpu'];
+                $curVapp['totals']['memory'] += $curVm['memory'];
+
+                
+                $diskZ = $sdkVm->getVirtualDisks();
+                
+                $myDisks = array();
+
+                foreach($diskZ->getItem() as $disk)
+                {  
+                    if(preg_match("/Hard\ disk/",$disk->getDescription()->valueOf))
+                    {
+                        
+                        $hostResource = $disk->getHostResource();
+                        $hr = $hostResource[0]->get_anyAttributes();
+
+                        $myDisks[] = $hr['capacity'];
+                        $curVm['totals']['storage'] += $hr['capacity'];                    
+                        $curVapp['totals']['storage'] += $hr['capacity'];                    
+                    }
+                }
+            
+                $curVm['storage'] = $myDisks;
+            
+            
+                $nics = $sdkVm->getVirtualNetworkCards();
+                
+                //ifDebug("\t\t\t\tNICS: " . $nics);
+                
+                $curVapp['vm'][] = $curVm;
+        }
+        $vApps[] = $curVapp;                
+    }
+    elseif(preg_match("/\.vAppTemplate\+/",$obj->get_type()))
+    {
+        $vAppTemplate['name'] = $obj->get_name();
+        $vAppTemplate['href'] = $obj->get_href();
+        $vAppTemplate['type'] = $obj->get_type();        
+        $vAppTemplates[] = $vAppTemplate;
+    }
+    elseif(preg_match("/\.media\+/",$obj->get_type()))
+    {
+        $media_item['name'] = $obj->get_name();
+        $media_item['href'] = $obj->get_href();
+        $media_item['type'] = $obj->get_type();
+        
+        $media[] = $media_item;
+    }
+    else
+    {
+        //print_r(array('name' => $obj->get_name, 'href'=> $obj->get_href(), 'type' => $obj->get_type()));
+    }
+
+}
+    
+    if($vApps || $vAppTemplates || $media)
+    {
+        return array('vApp' => $vApps, 'vAppTemplate' => $vAppTemplates, 'media' => $media);
+    }
+    else
+        return false;
+    
+}
+
+/* ************************************************************************** */
 function ifDebug($msg = "")
 {
     global $debug;
--- vStats.php
+++ vStats.php
@@ -1,10 +1,14 @@
 <?php
+require_once dirname(__FILE__) . '/config.php';
+require_once dirname(__FILE__) . '/simple-vCloud.php';
+
+// Organization name    /// Beware of this, case sensitive. Must match whats in vCD web interface.
+$org = 'AMC';    
 
 echo("<PRE>");
 
-require_once dirname(__FILE__) . '/config.php';
 
-require_once 'simple-vCloud.php';
+/* ************************************************************************* */
 
 $vCloud = VMware_VCloud_SDK_Service::getService();
 
@@ -13,126 +17,40 @@ if ( !($adminOrg = adminLogin($user, $pswd, $server, $org, $vCloud )) )
     die("Login Failed");
 }
 
-$vdcs = getVdcs($adminOrg,$vCloud,$org);
 
+
+$vdcs = getVdcs($adminOrg,$vCloud);
+
+// Loop through each VDC
 foreach($vdcs as $vdc)
 {
-    ifDebug("Processing Org VDC : " . $vdc->get_name());
-    
-    ifDebug("\tDescription : " . $vdc->getDescription());
-    ifDebug("\tStatus : " . $vdc->getIsEnabled());
-    ifDebug("\tAllocation Model: " . $vdc->getAllocationModel());
-    
-    ifDebug("\tCompute Capacity -- ");
-    ifDebug("\t\tCPU Size : " . $vdc->getVCpuInMhz() . "Mhz");
 
-    $computeCapacity = $vdc->getComputeCapacity();       
-
-    $cpuDetails = $computeCapacity->getCpu();
+    // Print each vDC's info
+    //print_r(getVdcInfo($vdc));
     
-    ifDebug("\t\tCPU Used : " . $cpuDetails->getUsed());
-    ifDebug("\t\tCPU Overhead : " . $cpuDetails->getOverhead());
-    ifDebug("\t\tCPU Reserved : " . $cpuDetails->getReserved());
-    
-    $ramDetails = $computeCapacity->getMemory();
-    
-    ifDebug("\t\tRAM Used : " . $ramDetails->getUsed());
-    ifDebug("\t\tRAM Overhead : " . $ramDetails->getOverhead());
-    ifDebug("\t\tRAM Reserved : " . $ramDetails->getReserved());   
-
-    $totals = array('ram' => 0, 'cpu' => 0, 'storage' => 0);
-    
-    $resourceEntities =  $vdc->getResourceEntities();
-    ifDebug();    
-foreach($resourceEntities->getResourceEntity() as $obj)
-{
-
-    if(preg_match("/vApp/",$obj->get_type()))
+    if($vdc)
     {
-
-        
-        $sdkvApp = $vCloud->createSDKObj($obj->get_href());
-        
-        $vApp = $sdkvApp->getVApp();
-        ifDebug("\t--- Resource :: vApp : " . $vApp->get_name());        
-        
-        $children = $vApp->getChildren();
-        
-        foreach($children->getVm() as $vm)
+        $resources =  getVdcResources($vCloud,$vdc);
+    
+        if($resources)
         {
 
+            echo("Displaying vApps\n-----------------------\n");
+            print_r($resources['vApp']);
 
-            ifDebug("\t\t\tVirtual Machine : " . $vm->get_name());
+            echo("\n\nDisplaying vAppTemplates\n-----------------------\n");
+            print_r($resources['vAppTemplate']);
             
-            $sdkVm = $vCloud->createSDKObj($vm->get_href());
-
-            $cpuZ = $sdkVm->getVirtualCpu();
-            $cpuY = $cpuZ->getVirtualQuantity();
-            $cpu = $cpuY->valueOf;
-
-            $memZ = $sdkVm->getVirtualMemory();
-            $memY = $memZ->getVirtualQuantity();
-            $mem = $memY->valueOf;            
-           
-            $diskZ = $sdkVm->getVirtualDisks();
-
-           
-            foreach($diskZ->getItem() as $disk)
-            {
-                
-  
-                if(preg_match("/Hard\ disk/",$disk->getDescription()->valueOf))
-                {
-                    $hostResource = $disk->getHostResource();
-                    $hr = $hostResource[0]->get_anyAttributes();
-                    ifDebug("\t\t\t\tHard Disk : " . $hr['capacity'] . "MB");
-                    $totals['storage'] += $hr['capacity'];
-                    
-                }
-                
-                
-            }
-            
-            
-            
-            
-            $nics = $sdkVm->getVirtualNetworkCards();
-            
-            ifDebug("\t\t\t\tCPU: " . $cpu . " vCPU");
-            ifDebug("\t\t\t\tMemory: " . $mem . "MB");
-            //ifDebug("\t\t\t\tDisks: " . $disks);
-            //ifDebug("\t\t\t\tNICS: " . $nics);
-
-            
-            $totals['cpu'] += $cpu;
-            $totals['ram'] += $mem;
+            echo("\n\nDisplaying Media\n-----------------------\n");
+            print_r($resources['media']);
 
         }
-        
-        
-    } elseif(preg_match("/media/",$obj->get_type()))
-    {
-        ifDebug("\t--- Resource :: Media Image : " . $obj->get_name());    
     }
-
-}
-    
-    //ifDebug("\t\Storage Used : " . $storageDetails->getUsed());
-    //ifDebug("\t\tStorage Overhead : " . $storageDetails->getOverhead());
-    //ifDebug("\t\tStorage Reserved : " . $storageDetails->getReserved());   
-    
-    ifDebug("\tMaximum VMs: " . $vdc->getVmQuota());    
-    ifDebug();
-
-    
-    ifDebug(print_r($totals,true));
 }
 
 
 
-
-
-
+// Logout
 if(isset($vCloud))
 {
 	// log out
